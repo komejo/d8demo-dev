8c266d315c0ad9e2f3b3a6c1c07c79c546d037b1
Joe Komenda <joe.komenda@thinkshout.com>
2015-09-08 11:17:40 -0700

    initial commit

---
+++ web/sites/default/files/php/twig/a7cde1e0_file-managed-file.html.twig_d81abde82eecf0ac78a42afe5057d6985c003d0d988cb61540fcbe42f0ebe324/a05e01ae42d03fef91927ffe3a5e0deb722cc136877fe8f3f710275e34292266.php
@@ -0,0 +1,46 @@
+<?php
+
+/* core/themes/classy/templates/content-edit/file-managed-file.html.twig */
+class __TwigTemplate_d81abde82eecf0ac78a42afe5057d6985c003d0d988cb61540fcbe42f0ebe324 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 14
+        $context["classes"] = array(0 => "js-form-managed-file", 1 => "form-managed-file");
+        // line 19
+        echo "<div";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => (isset($context["classes"]) ? $context["classes"] : null)), "method"), "html", null, true);
+        echo ">
+  ";
+        // line 20
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["element"]) ? $context["element"] : null), "html", null, true);
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/content-edit/file-managed-file.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  26 => 20,  21 => 19,  19 => 14,);
+    }
+}
